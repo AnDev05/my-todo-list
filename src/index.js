0d5f63990f2133d590578d902e8abff5db0b5a19
AnCT <anct@rikkeisoft.com>
2019-04-05 13:18:56 +0700

    init sign up page with firebase

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
-
 import Firebase, { FirebaseContext } from './components/firebase';
 
+require('dotenv').config();
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
