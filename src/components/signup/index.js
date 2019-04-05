80226a85d19bd5ca14aa22ca4d8505048d1473f2
AnCT <anct@rikkeisoft.com>
2019-04-05 14:11:53 +0700

    fix_error

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -32,7 +32,7 @@ class SignUpFormBase extends Component {
     onSubmit = event => {
         event.preventDefault();
         
-        const {username, email, passwordOne} = this.setState
+        const {username, email, passwordOne} = this.state
 
         this.props.firebase
         .doCreateUserWithEmailAndPassword(email, passwordOne)
